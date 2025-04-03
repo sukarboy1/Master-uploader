25007d23babb6db70b3f9e6104536997021e372f
sukarboy1 <rudrakashyap38@gmail.com>
2025-04-03 19:29:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7889827043:AAH5KTrUZ0m_XQLJrZ8h1mhWgHrahwYEy1E")  # Ensure correct key name
-    API_ID = int(os.environ.get("API_ID", "22581733"))  # Added key name and default value
-    API_HASH = os.environ.get("API_HASH", "1db7bdcf908100cc641c6a5276765c3d")  # Added key name for consistency
+    BOT_TOKEN = os.environ.get("7628579933:AAHNZ0hn5JG17JHPg3bWtsx_OCAtirKvJB8")  # Ensure correct key name
+    API_ID = int(os.environ.get("22581733"))  # Added key name and default value
+    API_HASH = os.environ.get("1db7bdcf908100cc641c6a5276765c3d")  # Added key name for consistency
 
-    AUTH_USER = os.environ.get("AUTH_USERS", "6530997270").split(',')
+    AUTH_USER = os.environ.get("AUTH_USERS", "7001787251").split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]  # Ensuring list of integers
 
     HOST = os.environ.get("HOST", "https://api.masterapi.tech")  # Keeping HOST configurable
